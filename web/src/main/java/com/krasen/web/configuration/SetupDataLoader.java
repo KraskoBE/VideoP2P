40632c254b65847b2d0eee7d472d539500fe412e
Krasen <krasenvachev3@gmail.com>
2022-09-02 13:10:03 +0300

    add spring profiles

--- web/src/main/java/com/krasen/web/configuration/SetupDataLoader.java
+++ web/src/main/java/com/krasen/web/configuration/SetupDataLoader.java
@@ -1,21 +1,17 @@
 package com.krasen.web.configuration;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
+import com.krasen.web.models.Role;
+import com.krasen.web.models.RoleType;
+import com.krasen.web.repositories.RoleRepository;
 import lombok.NonNull;
-import org.slf4j.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.MapPropertySource;
 import org.springframework.stereotype.Component;
 
-import com.krasen.web.models.*;
-import com.krasen.web.repositories.RoleRepository;
+import java.util.Arrays;
 
 @Component
 public class SetupDataLoader implements ApplicationListener<ContextRefreshedEvent> {
@@ -29,36 +25,8 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
     }
 
     @Override
-    public void onApplicationEvent(@NonNull ContextRefreshedEvent event ) {
+    public void onApplicationEvent( @NonNull ContextRefreshedEvent event ) {
         initializeRoles();
-        printActiveProperties((ConfigurableEnvironment) event.getApplicationContext().getEnvironment());
-    }
-
-    private void printActiveProperties(ConfigurableEnvironment env) {
-
-        System.out.println("************************* ACTIVE APP PROPERTIES ******************************");
-
-        List<MapPropertySource> propertySources = new ArrayList<>();
-
-        env.getPropertySources().forEach(it -> {
-            if (it instanceof MapPropertySource && it.getName().contains("applicationConfig")) {
-                propertySources.add((MapPropertySource) it);
-            }
-        });
-
-        propertySources.stream()
-                .map(propertySource -> propertySource.getSource().keySet())
-                .flatMap(Collection::stream)
-                .distinct()
-                .sorted()
-                .forEach(key -> {
-                    try {
-                        System.out.println(key + "=" + env.getProperty(key));
-                    } catch (Exception e) {
-                        logger.info("{} -> {}", key, e.getMessage());
-                    }
-                });
-        System.out.println("******************************************************************************");
     }
 
     private void initializeRoles() {
@@ -67,7 +35,7 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
     }
 
     private void createRoleIfNotFound( RoleType name ) {
-        if( roleRepository.findByName( name ).isEmpty() ) {
+        if ( roleRepository.findByName( name ).isEmpty() ) {
             roleRepository.save( Role.builder().name( name ).build() );
         }
     }
