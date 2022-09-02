4cf5e08029f1feabfbb0c8e2058e1a8959e7b2a8
Krasen <krasenvachev3@gmail.com>
2022-09-02 12:38:01 +0300

    add more debug logs

--- web/src/main/java/com/krasen/web/configuration/RestResponseEntityExceptionHandler.java
+++ web/src/main/java/com/krasen/web/configuration/RestResponseEntityExceptionHandler.java
@@ -1,36 +1,44 @@
 package com.krasen.web.configuration;
 
-import org.slf4j.*;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import com.krasen.web.exceptions.GenericException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.krasen.web.exceptions.GenericException;
+import java.util.Arrays;
 
 import static java.util.Objects.nonNull;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger( RestResponseEntityExceptionHandler.class );
+    private static final Logger logger = LoggerFactory.getLogger(RestResponseEntityExceptionHandler.class);
 
-    @ExceptionHandler( value = { RuntimeException.class } )
-    protected ResponseEntity<Object> handleGenericException( RuntimeException ex, WebRequest request ) {
-        logger.warn( "[{}]: {}", getPrincipalName( request ), ex.getMessage() );
-
-        return handleExceptionInternal( ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request );
+    @ExceptionHandler(value = {RuntimeException.class})
+    protected ResponseEntity<Object> handleRuntimeException(RuntimeException ex, WebRequest request) {
+        logger.warn("[{}]: {}", getPrincipalName(request), ex.getMessage());
+        logger.info(request.toString());
+        logger.info(Arrays.toString(ex.getStackTrace()));
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
-    @ExceptionHandler( value = { GenericException.class } )
-    protected ResponseEntity<Object> handleGenericException( GenericException ex, WebRequest request ) {
-        logger.warn( "[{}]: {}", getPrincipalName( request ), ex.getMessage() );
+    @ExceptionHandler(value = {GenericException.class})
+    protected ResponseEntity<Object> handleGenericException(GenericException ex, WebRequest request) {
+        logger.warn("[{}]: {}", getPrincipalName(request), ex.getMessage());
+        logger.info(request.toString());
+        logger.info(Arrays.toString(ex.getStackTrace()));
 
-        return handleExceptionInternal( ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request );
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
-    private String getPrincipalName( WebRequest request ) {
-        return nonNull( request.getUserPrincipal() ) ? request.getUserPrincipal().getName() : "No user";
+    private String getPrincipalName(WebRequest request) {
+        return nonNull(request.getUserPrincipal()) ? request.getUserPrincipal().getName() : "No user";
     }
 
 }
